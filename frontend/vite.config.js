fbb1c0eef0d9cc9e1ba6fa41ed21567b3b949d4f
SVBSH <getenv@protonmail.com>
2024-07-27 18:33:26 +0200

    add proxy to local server in development mode

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  let server = {};
+  if (mode === "development") {
+    server = {
+      host: "0.0.0.0",
+      port: 5173,
 
-  // https://stackoverflow.com/questions/70934834/eslint-complains-about-dirname-not-being-defined-in-a-nodejs-file
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@context': path.resolve(__dirname, './src/context')
-    }
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      },
+    };
+  } else {
+    server = {
+      server: {
+        host: "0.0.0.0",
+        port: 5173,
+      },
+    };
   }
-})
+  return {
+    plugins: [react()],
+    resolve: {
+      // https://stackoverflow.com/questions/70934834/eslint-complains-about-dirname-not-being-defined-in-a-nodejs-file
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@services": path.resolve(__dirname, "./src/services"),
+        "@context": path.resolve(__dirname, "./src/context"),
+      },
+    },
+    server: server,
+  };
+});
